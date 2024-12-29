82f1ae84e7f9e38b10203117c0180cfcd08aaeb9
Gibbs155 <alejandrob1l@hotmail.com>
2024-12-28 19:02:27 -0500

    first commit

---
+++ src/main/java/com/example/sprinbott_example_deploy/SprinbottExampleDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbott_example_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbottExampleDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbottExampleDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprinbott_example_deploy/SprinbottExampleDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbott_example_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbottExampleDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
